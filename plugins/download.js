879955a93680d0aa4de7fde59d7b3d9de26dad0e
manaofc <manisha1718manisha@gmail.com>
2026-05-18 22:37:49 +0530

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -2,7 +2,7 @@ const { File } = require("megajs");
 const apkdl = require('../lib/apkdl');
 const { fetchJson } = require('../lib/functions')
 const { cmd } = require("../command");
-const config = require("./config");
+const config = require("../config");
 const path = require("path");
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const axios = require("axios");
