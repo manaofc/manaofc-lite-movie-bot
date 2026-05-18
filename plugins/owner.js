51c71f470070d2ad0ca9b6785d01453dd0e16122
manaofc <manisha1718manisha@gmail.com>
2026-05-18 17:49:20 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,31 @@
+cmd({
+    pattern: "owner",
+    desc: "Display owner contact information.",
+    react: "🌝",
+    use: ".owner",
+    category: "owner",
+    filename: __filename
+},
+async (socket, mek, m, { from, reply }) => {
+    try {
+        const vcard = 
+            'BEGIN:VCARD\n' +
+            'VERSION:3.0\n' +
+            'FN:MANAOFC\n' +
+            'ORG:MANAOFC\n' +
+            'TEL;type=CELL;type=VOICE;waid=94759934522:+94759934522\n' +
+            'EMAIL:manishasasmith27@gmail.com\n' +
+            'END:VCARD';
+
+        await socket.sendMessage(from, { 
+            contacts: { 
+                displayName: "manaofc", 
+                contacts: [{ vcard }] 
+            },  
+            quoted: mek 
+        });
+    } catch (e) {
+        console.error(e);
+        reply('⚠️ An error occurred while fetching owner information.');
+    }
+});
