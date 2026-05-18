97d287ac616631da7c475026931e41cd5fbbd4ea
manaofc <manisha1718manisha@gmail.com>
2026-05-18 18:36:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ const defaultConfig = {
     MAX_RETRIES: 3,
     WORK_TYPE: 'private',
     ADMIN_LIST_PATH: './admin.json',
-    IMAGE_PATH: 'https://files.catbox.moe/qlx0lp.png',
-    OWNER_NUMBER: '94759934522'
+    IMAGE_PATH: 'https://files.catbox.moe/qlx0lp.png'
 };
