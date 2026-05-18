07485ac8af3d8a104e77f739a149b55fdad0f0d7
manaofc <manisha1718manisha@gmail.com>
2026-05-18 17:26:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-
+const defaultConfig = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['💥', '👍', '😍', '💗', '🎈', '🎉', '🥳', '😎', '🚀', '🔥'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    WORK_TYPE: 'private',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://files.catbox.moe/qlx0lp.png',
+    OWNER_NUMBER: '94759934522'
+};
