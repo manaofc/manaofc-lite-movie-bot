511a298a9ce17097a3f2be774d867795b2a8b326
manaofc <manisha1718manisha@gmail.com>
2026-05-18 17:48:58 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -383,38 +383,7 @@ ${msgData.footer}`;
 ////////////// OWNER COMMAND ////////////////
             ////////////////////
 
-  cmd({
-    pattern: "owner",
-    desc: "Display owner contact information.",
-    react: "🌝",
-    use: ".owner",
-    category: "owner",
-    filename: __filename
-},
-async (socket, mek, m, { from, reply }) => {
-    try {
-        const vcard = 
-            'BEGIN:VCARD\n' +
-            'VERSION:3.0\n' +
-            'FN:MANAOFC\n' +
-            'ORG:MANAOFC\n' +
-            'TEL;type=CELL;type=VOICE;waid=94759934522:+94759934522\n' +
-            'EMAIL:manishasasmith27@gmail.com\n' +
-            'END:VCARD';
-
-        await socket.sendMessage(from, { 
-            contacts: { 
-                displayName: "manaofc", 
-                contacts: [{ vcard }] 
-            },  
-            quoted: mek 
-        });
-    } catch (e) {
-        console.error(e);
-        reply('⚠️ An error occurred while fetching owner information.');
-    }
-});
-
+  
               ///////////////////
 ////////////// MOVIE COMMAND ////////////////
             ////////////////////
