50e277bc00f6cd9daf371fc885c1cee9a53668bd
manaofc <manisha1718manisha@gmail.com>
2026-05-18 17:32:22 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -436,40 +436,7 @@ ${msgData.footer}`;
 /////// SEARCH COMMAND ////////
       /////////////////
 
-function ytreg(url) {
-    const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed|shorts\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
-    return ytIdRegex.test(url);
-}
-cmd({
-    pattern: "yts",
-    alias: ["y"],
-    use: ".yts ",
-    react: "🔎",
-    desc: "Search Youtube Songs or Videos.",
-    category: "search",
-    filename: __filename
 
-},
-async (socket, mek, m, { from, q, reply }) => {
-try{
-if (!q) return await reply(imgmsg)
-if(isUrl(q) && !ytreg(q)) return await reply(imgmsg)
-try {
-var arama = await yts(q);
-} catch(e) {
-    l(e)
-return await socket.sendMessage(from , { text: '*Error !!*' }, { quoted: mek } )
-}
-var mesaj = '';
-arama.all.map((video) => {
-mesaj += ' *◈ ' + video.title + '*\n🔗 ' + video.url + '\n\n> _*Powered By Manaofc*_ '
-});
-await socket.sendMessage(from , { text:  mesaj }, { quoted: mek } )
-} catch (e) {
-    l(e)
-  reply('*Error !!*')
-}
-})
 
          /////////////////
   ////// CONVART COMMAND //////
